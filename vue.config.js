078d9727b7278df4de9b51161060d5fedda9f888
Your Name <you@example.com>
2020-08-02 09:49:32 +0800

    weLight

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    lintOnSave: false, //是否开启eslint
+    devServer: {
+        open: true, //是否自动弹出浏览器页面
+        https: false,
+        hotOnly: false, 
+        proxy: {
+            '/api': {
+                target: 'http://47.97.214.211/api',
+                // target:'http://127.0.0.1:3000/login',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        },
+    }
+}
