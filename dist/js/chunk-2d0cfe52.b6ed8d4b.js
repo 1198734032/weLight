0b5d16ca3d04114c6eb389e46b473f5bbb6a7649
Your Name <you@example.com>
2020-08-06 10:10:13 +0800

    a test

---
+++ dist/js/chunk-2d0cfe52.b6ed8d4b.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0cfe52"],{6651:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[e._v("这是用户创建房间的页面")])},u=[],a={},r=a,s=t("2877"),i=Object(s["a"])(r,c,u,!1,null,"48fe34ad",null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0cfe52.b6ed8d4b.js.map
